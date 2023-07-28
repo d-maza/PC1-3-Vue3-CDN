14ca5a70e89c6c93e8a7121cd8649bddcd7fc6d0
David Maza <david.maza@hotmail.es>
2023-07-28 18:09:22 +0200

    segundo commmit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const app = Vue.createApp({
+
   data() {
-      return { 
-        imagen:'./img/plama.png', 
-        gitmefive:'Git me five',
-        saludo: 'Chocala, para continuar...', 
-        text: 'Crea una App Vue, que tendrá un título, subtitulo, un párrafo. Añadiremos una fotografía en la página desde el lado del JS y después saludaremos al usuario que también lo pararemos los datos desde el JS',
-        usuario: 'Teacher ',
-        textSaludo: 'Hello!!! Te saludo desde Vue 👋🏽'
-        
-      }
+    return {
+      imagen: './img/plama.png',
+      gitmefive: 'Git me five',
+      saludo: 'Chocala, para continuar...',
+      text: 'Crea una App Vue, que tendrá un título, subtitulo, un párrafo. Añadiremos una fotografía en la página desde el lado del JS y después saludaremos al usuario que también lo pararemos los datos desde el JS',
+      textSaludo: ' Hello!!! Te saludo desde Vue 👋🏽'
+
+    }
   },
   methods: {
     chocala() {
-        
-        this.imagen='./img/DiveControl.png',
-        this.gitmefive=this.usuario+this.textSaludo 
-        this.saludo = '',
-        this.text = 'Have a nice day 🤓'
+
+      this.imagen = './img/DiveControl.png',
+      this.gitmefive = this.textSaludo
+      this.saludo = '',
+      this.text = 'Have a nice day 🤓'
     }
-},
+  },
 
 })
 
